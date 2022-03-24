6b5be722e4bba6ffce822223f73cf7745b836a69
BigPandaCPU <1196449387@qq.com>
2022-03-24 13:41:21 +0800

    修改readme

--- 面试常考算法题/动态规划之背包问题.py
+++ 面试常考算法题/动态规划之背包问题.py
@@ -7,8 +7,6 @@ bag = [[0 for i in range(weight_most+1)] for j in range(len(weight))]
 for i in range(1, len(weight)):
     for j in range(1, weight_most+1):
         if j >= weight[i]:
-            #if (bag[i-1][j-weight[i]]+value[i] > bag[i-1][j]):
-                #wuping.append(i)
             bag[i][j] = max(bag[i-1][j-weight[i]]+value[i], bag[i-1][j])
         else:
             bag[i][j] = bag[i-1][j]
@@ -16,11 +14,13 @@ for j in range(len(weight)):
     print(bag[j])
 
 
+wuping.clear()
+
 j = weight_most
 i = len(weight)-1
 while(j>0):
     if bag[i][j] != bag[i-1][j]: #判断第i见物品，被装进背包
-        wuping.append(i)
+        wuping.append(weight[i])
         j = j-weight[i]
         i -=1
     else: #没有被装进背包
@@ -32,14 +32,17 @@ print(wuping)
 #print(wuping)
 
 #背包里的物品
-# weight = [2, 3, 4, 4, 1]
-# value =  [2, 6, 4, 5, 3]
-# weight_most=10
-# dp = [0]*(weight_most+1)
-# #print(dp)
-#
-# for i in range(len(weight)):
-#     for j in range(weight_most, weight[i]-1, -1):
-#         dp[j] = max(dp[j-weight[i]]+value[i], dp[j])
-#
-#     #print(dp)
+weight = [2, 3, 4, 4, 1]
+value =  [2, 6, 4, 5, 3]
+
+weight = [2, 3, 4, 4, 1, 5, 6]
+value =  [2, 6, 4, 5, 3, 8, 9]
+weight_most=15
+dp = [0]*(weight_most+1)
+#print(dp)
+
+for i in range(len(weight)):
+    for j in range(weight_most, weight[i]-1, -1):  #只有一个物品的时候，背包所装的最大价值就是该物品的价值
+        dp[j] = max(dp[j-weight[i]]+value[i], dp[j])
+
+    print(dp)
